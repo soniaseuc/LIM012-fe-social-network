88016961c3abf8431a1a749146c3282e558fa209
paula113 <paulapierina96@gmail.com>
2020-04-22 17:00:55 -0500

    resolve #6 implementacion de 404 SPA sin css

--- src/view/index.js
+++ src/view/index.js
@@ -2,12 +2,13 @@ import SignUpform from './signUp.js';
 import LogInform from './logIn.js';
 // import Home from './home.js';
 // import Profile from './profile.js';
-// import Different from './404.js';
+import Different from './404.js';
 
 
 const components = {
   logInform: LogInform,
   signUpform: SignUpform,
+  different: Different,
 
 };
 
