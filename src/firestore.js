f1bcb18756b58b6cc5b3875f5061469ba10bc5f6
sonia.seuc <sonia.seuc@gmail.com>
2020-05-11 20:56:45 -0500

    mostrar delete edit cuando es privado o publico pero solo del currentUser NO funciona bien el editar

--- src/firestore.js
+++ src/firestore.js
@@ -25,8 +25,9 @@ const deletePublication = (e) => {
 
 // FUNCIÓN PARA ACTUALIZAR LOS POSTS
 const editNote = (idDoc, pElementToEdit) => {
+  console.log('dentro de la funcion editNote');
   document.querySelector('#input-edit-note').value = pElementToEdit;
-  const textareaEdited = document.querySelector('#inputPost').value;
+  const textareaEdited = document.querySelector(`#inputPost-${idDoc}`).value;
   firebase.firestore().collection('post').doc(idDoc).update({
     status: textareaEdited,
     // img: file,
@@ -55,9 +56,9 @@ export const publishStatus = (userName, statusPost, visibilityPost, imgPost, uid
     img: imgPost,
   })
     .then((docRef) => {
-      console.log(`'currentUser.uid written with ID: ${uid}`);
-      console.log(`'Document written with ID: ${docRef.id}`);
-      // console.log(docRef.visibility);
+      // console.log(`'currentUser.uid written with ID: ${uid}`);
+      // console.log(`'Document written with ID: ${docRef.id}`);
+      console.log(docRef.visibility);
       // console.log(docRef.id);
       document.querySelector('[placeholder="¿Que quieres compartir?"]').value = '';
     })
@@ -74,56 +75,58 @@ export const publishStatus = (userName, statusPost, visibilityPost, imgPost, uid
 * READ DATABASE
 */
 
-const validatePost = (img, post) => {
+const validatePost = (img, post, idDoc) => {
 // B/C THERE WAS AN BROKEN IMG ON EACH PUBLISHED POST
   let postTemplate = '';
   if (img) {
     postTemplate = `
     <p class="textComent" id="input-edit-note">${post}</p>
-    <textarea id="inputPost" class="displayNone">${post}</textarea>
+    <textarea id="inputPost-${idDoc}" class="displayNone">${post}</textarea>
     <img  class="postedImg" src="${img}">
     `;
   } else {
     postTemplate = `
     <p class="textComent" id="input-edit-note">${post}</p>
-    <textarea id="inputPost" class="displayNone">${post}</textarea>
+    <textarea id="inputPost-${idDoc}" class="displayNone">${post}</textarea>
     `;
   }
   return postTemplate;
 };
 
-
-const ifPublicButMine = (doc, userId) => {
-  console.log('dentro de ifPublivButMine');
-  const postDeleteEdit = document.getElementById('ifPublicButMine');
-  const currentUserUid = firebase.auth().currentUser;
-  // const postDeleteEdit = document.createElement('section');
-  // postDeleteEdit.innerHTML = '';
-  console.log(`${userId} =?= ${currentUserUid.uid}`);
-  if (userId === currentUserUid.uid) {
-    console.log(`dentro de ${userId} == ${currentUserUid.uid}`);
-    const post = document.createElement('div');
-    post.innerHTML = '';
-    post.innerHTML += `
-    <figure class="figureContainerIcons">
-    <input id="delete-${doc.id}" type="checkbox">
-    <label for="delete-${doc.id}">
-        <img src="img/icons/trash.svg">
-    </label>
-  </figure>
-  <figure class="figureContainerIcons">
-    <input id="edit-${doc.id}" type="checkbox">
-    <label for="edit-${doc.id}">
-        <img src="img/icons/modificar.svg">
-    </label>
-  </figure>
-  `;
-    postDeleteEdit.appendChild(post);
-  } else {
-    console.log('dentro del else ifPublicButMine');
-  }
-  return postDeleteEdit;
-};
+// FUNCION PARA MOSTRAR EL BOTON EDITAR Y ELIMINAR CUANDO ES PUBLICO
+// ${ifPublicButMine(doc.id, doc.data().id)}
+// PORQUE ANTES SOLO MOSTRABA ESOS BOTONES CUANDO ERA PRIVADO
+// const ifPublicButMine = (doc, userId) => {
+//   console.log('dentro de ifPublivButMine');
+//   const postDeleteEdit = document.getElementById('ifPublicButMine');
+//   const currentUserUid = firebase.auth().currentUser;
+//   // const postDeleteEdit = document.createElement('section');
+//   // postDeleteEdit.innerHTML = '';
+//   console.log(`${userId} =?= ${currentUserUid.uid}`);
+//   if (userId === currentUserUid.uid) {
+//     console.log(`dentro de ${userId} == ${currentUserUid.uid}`);
+//     const post = document.createElement('section');
+//     post.innerHTML += `
+//     <figure class="figureContainerIcons">
+//     <input id="delete-${doc.id}" type="checkbox">
+//     <label for="delete-${doc.id}">
+//         <img src="img/icons/trash.svg">
+//     </label>
+//   </figure>
+//   <figure class="figureContainerIcons">
+//     <input id="edit-${doc.id}" type="checkbox">
+//     <label for="edit-${doc.id}">
+//         <img src="img/icons/modificar.svg">
+//     </label>
+//   </figure>
+//   `;
+//     postDeleteEdit.innerHTML = post;
+//   } else {
+//     console.log('dentro del else ifPublicButMine');
+//     // postDeleteEdit.innerHTML = '';
+//   }
+//   return postDeleteEdit;
+// };
 
 export const getStatus = () => {
   const mainElem = document.getElementById('mainElement');
@@ -136,8 +139,8 @@ export const getStatus = () => {
     .onSnapshot((querySnapShot) => {
       statusPost.innerHTML = '';
       querySnapShot.forEach((doc) => {
-        // console.log(`post Id => ${doc.id} | usuario Id = ${doc.data().id}`);
-        if (doc.data().visibility === 'public') {
+        console.log(`postId = ${doc.id} | usuerId = ${doc.data().id} | status: ${doc.data().status}`);
+        if (doc.data().visibility === 'public' && doc.data().id !== currentUserUid.uid) {
         // B/C PUBLIC STATUS SHOULD BE DISPLAY TO EVERYONE
           const post = document.createElement('section');
           post.className = 'publicationSection';
@@ -150,17 +153,15 @@ export const getStatus = () => {
                     <option value="private">Private</option>
                 </select>
                 <h1 class="nameTitlePublication">${doc.data().name} </h1>
-                <div id="ifPublicButMine">${ifPublicButMine(doc.id, doc.data().id)}</div>
-                
+                <div id="ifPublicButMine"></div>
             </header>
 
             <section class="notes" id="content">
-                ${validatePost(doc.data().img, doc.data().status)}
+                ${validatePost(doc.data().img, doc.data().status, doc.id)}
                 <p class="softFont">Publicado ${doc.data().date.toDate()}</p>
                 <div class="notesIcons">
                 <button id="likeHeart" class="circlePink"><img src="img/icons/heart-solid.svg"></button>
                 <button id="likeHeart" class="circlePink"><img src="img/icons/comments.svg"></button>
-                <button id="btnSaveEdit" class="cambioBtn">Guardar Cambio</button>
                 </div>
             </section>
             <section class="comment" id="comments">
@@ -183,7 +184,7 @@ export const getStatus = () => {
           statusPost.appendChild(post);
         } if (doc.data().visibility === 'private' && doc.data().id === currentUserUid.uid) {
         // B/C PRIVATE ESTATUS CAN ONLY BE SEEN BY THE CURRENT USER
-          console.log(`HOLA ${currentUserUid.uid} MI POST ES ${doc.data().status}`);
+          // console.log(`HOLA ${currentUserUid.uid} MI POST ES ${doc.data().status}`);
           const post = document.createElement('section');
           post.className = 'publicationSection';
 
@@ -209,12 +210,12 @@ export const getStatus = () => {
                     </figure>
                 </header>
                 <section class="notes" id="content">
-                    ${validatePost(doc.data().img, doc.data().status)}
+                    ${validatePost(doc.data().img, doc.data().status, doc.id)}
                     <p class="softFont">Publicado ${doc.data().date.toDate()}</p>
                     <div class="notesIcons">
                     <button id="likeHeart" class="circlePink"><img src="img/icons/heart-solid.svg"></button>
                     <button id="likeHeart" class="circlePink"><img src="img/icons/comments.svg"></button>
-                    <button id="btnSaveEdit" class="cambioBtn">Guardar Cambio</button>
+                    <button id="btnSaveEdit-${doc.id}" class="cambioBtn">Guardar Cambio</button>
                     </div>
                 </section>
                 <section class="comment" id="comments">
@@ -235,12 +236,66 @@ export const getStatus = () => {
                 </section>
                 `;
           statusPost.appendChild(post);
+        } if (doc.data().visibility === 'public' && doc.data().id === currentUserUid.uid) {
+          // B/C PRIVATE ESTATUS CAN ONLY BE SEEN BY THE CURRENT USER
+          // console.log(`HOLA ${currentUserUid.uid} MI POST ES ${doc.data().status}`);
+          const post = document.createElement('section');
+          post.className = 'publicationSection';
+
+          post.innerHTML += `
+
+                  <header>
+                      <select id="" class="publicOrPrivateSelector">
+                          <option value="public">Public</option>
+                          <option value="private">Private</option>
+                      </select>
+                      <h1 class="nameTitlePublication">${doc.data().name} </h1>
+                      <figure class="figureContainerIcons">
+                        <input id="delete-${doc.id}" type="checkbox">
+                        <label for="delete-${doc.id}">
+                            <img src="img/icons/trash.svg">
+                        </label>
+                      </figure>
+                      <figure class="figureContainerIcons">
+                        <input id="edit-${doc.id}" type="checkbox">
+                        <label for="edit-${doc.id}">
+                            <img src="img/icons/modificar.svg">
+                        </label>
+                      </figure>
+                  </header>
+                  <section class="notes" id="content">
+                      ${validatePost(doc.data().img, doc.data().status, doc.id)}
+                      <p class="softFont">Publicado ${doc.data().date.toDate()}</p>
+                      <div class="notesIcons">
+                      <button id="likeHeart" class="circlePink"><img src="img/icons/heart-solid.svg"></button>
+                      <button id="likeHeart" class="circlePink"><img src="img/icons/comments.svg"></button>
+                      <button id="btnSaveEdit-${doc.id}" class="cambioBtn">Guardar Cambio</button>
+                      </div>
+                  </section>
+                  <section class="comment" id="comments">
+                      <div class="userComentDone">
+                        <div class="flexColumn">
+                          <h5>NOMBRE</h5>
+                          <p>Comentario......</p>
+                        </div>
+                        <div class="icons">
+                          <button id="likeHeart" class="circlePink"><img src="img/icons/modificar.svg"></button>
+                          <button id="likeHeart" class="circlePink"><img src="img/icons/trash.svg"></button>
+                          <button id="likeHeart" class="circlePink"><img src="img/icons/heart-solid.svg"></button>
+                        </div>
+                      </div>
+                      <div class="line">
+                      </div>
+                      <input placeholder="Agrega tu Comentario"></input>
+                  </section>
+                  `;
+          statusPost.appendChild(post);
         }
         // agregando evento de click al btn eliminar un post
         const btnDeleted = statusPost.querySelector(`#delete-${doc.id}`);
 
         if (btnDeleted) {
-          console.log(btnDeleted);
+          // console.log(btnDeleted);
           btnDeleted.addEventListener('click', () => {
             deletePublication(doc.id);
           });
@@ -249,8 +304,8 @@ export const getStatus = () => {
         // FUNCIONES PARA EDITAR PUBLICACION
         const modificar = statusPost.querySelector(`#edit-${doc.id}`);
         const post = statusPost.querySelector('#input-edit-note');
-        const btnEdit = document.getElementById('btnSaveEdit');
-        const inputPost = statusPost.querySelector('#inputPost');
+        const btnEdit = document.getElementById(`btnSaveEdit-${doc.id}`);
+        const inputPost = statusPost.getElementById(`inputPost-${idDoc}`);
 
         if (modificar) {
           console.log(modificar);
@@ -261,9 +316,10 @@ export const getStatus = () => {
           });
         }
         if (btnEdit) {
+          console.log(btnEdit);
           // agregando evento de click al btn guardar cambio en la publicacion
           btnEdit.addEventListener('click', () => {
-            editNote(doc.id, post.value);
+            editNote(`${doc.id}`, post.value);
           });
         }
       });
