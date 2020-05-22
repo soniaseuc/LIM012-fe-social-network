0ade61917a32ed548a0ed844606ae06d4495d306
sonia.seuc <sonia.seuc@gmail.com>
2020-05-22 18:46:57 -0500

    borrando comentarios de varios archivos

--- src/firestore-controller/firebase-storage.js
+++ src/firestore-controller/firebase-storage.js
@@ -3,7 +3,5 @@
 export const uploadImagePost = (file, uid) => {
   const refStorage = firebase.storage().ref(`imgPost/${uid}/${file.name}`);
   refStorage.put(file);
-  // console.log(`soy file de firestore.js ${refStorage}`);
-  // console.log(`refStorage.put(file) ${refStorage.put(file)}`);
   return refStorage;
 };
--- test/firebase-storage.spec.js
+++ test/firebase-storage.spec.js
@@ -1,7 +1,4 @@
-// Now mock 'firebase`:
 import firebasemock from 'firebase-mock';
-
-// import getStatus from '../src/firestore-controller/firestore.js';
 
 const mockauth = new firebasemock.MockAuthentication();
 global.firebase = firebasemock.MockFirebaseSdk(
@@ -9,15 +6,6 @@ global.firebase = firebasemock.MockFirebaseSdk(
   () => null,
   () => mockauth,
 );
-// jest.mock('../src/firestore-controller/firebase-storage.js', () => {
-//   const mockfirestore = new firebasemock.MockFirestore();
-//   return new firebasemock.MockFirebaseSdk(
-//     null, // RTDB
-//     () => mockauth,
-//     () => mockfirestore,
-//   );
-// });
-
 
 // eslint-disable-next-line import/first
 import { uploadImagePost } from '../src/firestore-controller/firebase-storage.js';
@@ -31,14 +19,6 @@ const img = {
   uid: '38cj4',
 };
 
-// const user = {
-//   file: 'user.png',
-//   uid: '38cj4',
-// };
-// console.log(uploadImagePost(img.file, img.uid));
-// console.log();
-
-
 it('Deberia poder subir una img al storage', (done) => {
   const response = uploadImagePost(img.file, img.uid);
   console.log(response.fullPath);
@@ -51,35 +31,4 @@ it('Deberia poder subir una img al storage', (done) => {
     // done(error);
     console.log(error.message);
   }
-  // test('the data is peanut butter', done => {
-  // function callback(data) {
-  //   try {
-  //     expect(data).toBe('peanut butter');
-  //     done();
-  //   } catch (error) {
-  //     done(error);
-  //   }
-  // }
-
-  // fetchData(callback);
 });
-
-// .then(() => {
-//   // console.log(response);
-//   // const result = data.find(note => note.img === `imgPost/${img.uid}/${img.file}`);
-//   expect('imgPost/38cj4/hola.png').toEqual(`imgPost/${img.uid}/${img.file}`);
-//   done();
-// })
-// .catch((error) => {
-//   // The document probably doesn't exist.
-//   console.error('Error updating document: ', error);
-// }));
-
-// it('Deberia poder subir una img al storage', done => uploadImagePost(img.file, img.uid)
-//   .then(() => getStatus(
-//     (data) => {
-//       const result = data.find(note => note.img === `imgPost/${img.uid}/${img.file}`);
-//       expect(result.file.name).toEqual();
-//       done();
-//     },
-//   )));
